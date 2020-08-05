ccd18d2dd3c6ceb2b7b8a65b03992f64ab654ce3
nine-git <15397725059@163.com>
2020-08-05 11:06:17 +0800

    feat:init vue项目

---
+++ src/api/api.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-template-curly-in-string */
+import http from "../unit/http";
+// import cookie from 'js-cookie'
+let youYiHeader = {
+  Authorization:
+    "760aa4f94594a1e58352a288021fb90cd0ae29bea865ca0c106e0054c2d7fea4:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lc3BhY2VfaWQiOjd9._oYNdkJna4jaB-SzMZ4edpydgpwrhVPEPTcEZxTHRwo"
+};
+let gxHeaders = {
+  Authorization:
+    "d25adaf98146c4f32c127e6c094f6f672db695970692fc473a9e8e861b304f1a:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lc3BhY2VfaWQiOjF9.plo6dlWfBGCS9kH8765wWR9aQo_-SyFQw5kRBBio9Y0"
+};
+export default {
+
+};
